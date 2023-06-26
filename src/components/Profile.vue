60a6d5e6db639d30bf48403b594c31a91613cc98
Łukasz Beczek <lb@turis.pl>
2023-06-26 13:54:09 +0200

    Complex data relations

--- src/components/Profile.vue
+++ src/components/Profile.vue
@@ -1,70 +1,117 @@
 <script setup>
 import Container from './Container.vue'
-import ImageGallery from './ImageGallery.vue';
+import ImageGallery from './ImageGallery.vue'
 import UserBar from './UserBar.vue'
-import {ref, onMounted} from 'vue'
-import {supabase} from '../supabase'
-import {useRoute} from 'vue-router'
+import { ref, onMounted, watch, reactive } from 'vue'
+import { supabase } from '../supabase'
+import { useRoute } from 'vue-router'
+import { useUserStore } from '../stores/users.js'
+import { storeToRefs } from 'pinia'
 
 const route = useRoute()
+const userStore = useUserStore()
+const { username } = route.params
+const { user: loggedInUser } = storeToRefs(userStore)
+
 const user = ref(null)
-const {username} = route.params
+const isFollowing = ref(false)
 const posts = ref([])
 const loading = ref(false)
+const userInfo = reactive({
+	posts: null,
+	followers: null,
+	following: null,
+})
 
-const addNewPost = (post) => {
-    posts.value.unshift(post)
+const addNewPost = post => {
+	posts.value.unshift(post)
+}
+
+const updateIsFollowing = follow => {
+	isFollowing.value = follow
 }
 
 const fetchData = async () => {
-    loading.value = true
-    const {data: userData} = await supabase
-    .from('users')
-    .select()
-    .eq('username', username)
-    .single()
+	loading.value = true
+	const { data: userData } = await supabase.from('users').select().eq('username', username).single()
 
-    if(!userData) {
-        loading.value = false
-        return user.value = null
-    }
+	if (!userData) {
+		loading.value = false
+		return (user.value = null)
+	}
 
-    user.value = userData
-    
-    const {data: postsData} = await supabase
-    .from('posts')
-    .select()
-    .eq('owner_id', user.value.id)
+	user.value = userData
 
-    posts.value = postsData
+	const { data: postsData } = await supabase.from('posts').select().eq('owner_id', user.value.id)
 
-    loading.value = false
+	posts.value = postsData
+
+	await fetchIsFollowing()
+
+	const followerCount = await fetchFollowersCount()
+    const followingCount = await fetchFollowingCount()
+
+    userInfo.followers = followerCount;
+    userInfo.following = followingCount;
+    userInfo.posts = posts.value.length
+
+	loading.value = false
 }
 
-onMounted(() => {
-    fetchData()
-})
+const fetchFollowersCount = async () => {
+	const {count} = await supabase
+		.from('followers_following')
+		.select('*', { count: 'exact' })
+		.eq('following_id', user.value.id)
 
+	return count
+}
+
+const fetchFollowingCount = async () => {
+	const {count} = await supabase
+		.from('followers_following')
+		.select('*', { count: 'exact' })
+		.eq('follower_id', user.value.id)
+
+	return count
+}
+
+const fetchIsFollowing = async () => {
+	if (loggedInUser.value && loggedInUser.value.id !== user.value.id) {
+		const { data } = await supabase
+			.from('followers_following')
+			.select()
+			.eq('follower_id', loggedInUser.value.id)
+			.eq('following_id', user.value.id)
+			.single()
+
+		if (data) isFollowing.value = true
+	}
+}
+
+watch(loggedInUser, () => {
+	fetchIsFollowing()
+})
+onMounted(() => {
+	fetchData()
+})
 </script>
 
 <template>
 	<Container>
 		<div class="profile-container" v-if="!loading">
 			<UserBar
-            :key="$route.params.username"
-            :user="user"
-            :userInfo="{
-                posts: 4,
-                followers: 100,
-                following: 342
-            }"
-            :addNewPost="addNewPost"
-            />
-            <ImageGallery :posts="posts"/>
+				:key="$route.params.username"
+				:user="user"
+				:userInfo="userInfo"
+				:addNewPost="addNewPost"
+				:isFollowing="isFollowing"
+				:updateIsFollowing="updateIsFollowing" />
+			<ImageGallery :posts="posts" />
 		</div>
-        <div class="spinner" v-else>
-            <a-spin />
-        </div>
+		<div class="spinner" v-else>
+			<a-spin />
+		</div>
 	</Container>
 </template>
 
@@ -77,9 +124,9 @@ onMounted(() => {
 }
 
 .spinner {
-    display: flex;
-    align-items: center;
-    justify-content: center;
-    height: 85vh;
+	display: flex;
+	align-items: center;
+	justify-content: center;
+	height: 85vh;
 }
 </style>
--- src/components/UserBar.vue
+++ src/components/UserBar.vue
@@ -1,29 +1,49 @@
 <script setup>
-import {defineProps} from 'vue'
-import UploadPhotoModal from './UploadPhotoModal.vue';
-import {useRoute} from 'vue-router'
-import {useUserStore} from '../stores/users.js'
-import { storeToRefs } from 'pinia';
+import { defineProps } from 'vue'
+import UploadPhotoModal from './UploadPhotoModal.vue'
+import { useRoute } from 'vue-router'
+import { useUserStore } from '../stores/users.js'
+import { storeToRefs } from 'pinia'
+import { supabase } from '../supabase'
 
 const route = useRoute()
 const userStore = useUserStore()
 
-const {user} = storeToRefs(userStore)
-const {username: profileUsername} = route.params 
+const { user } = storeToRefs(userStore)
+const { username: profileUsername } = route.params
 
-const props = defineProps(['user','userInfo', 'addNewPost'])
+const props = defineProps(['user', 'userInfo', 'addNewPost', 'isFollowing', 'updateIsFollowing'])
+
+const followUser = async () => {
+	props.updateIsFollowing(true)
+	await supabase.from('followers_following').insert({
+		follower_id: user.value.id,
+		following_id: props.user.id
+	})
+}
+
+const unfollowUser = async () => {
+	props.updateIsFollowing(false)
+	await supabase.from('followers_following').delete().eq('follower_id', user.value.id).eq('following_id', props.user.id)
+}
 </script>
 
 <template>
 	<div class="userbar-container" v-if="props.user">
 		<div class="top-content">
-			<a-typography-title :level="2">{{props.user.username}}</a-typography-title>
-			<UploadPhotoModal v-if="user && profileUsername === user.username" :addNewPost="addNewPost"/>
+			<a-typography-title :level="2">{{ props.user.username }}</a-typography-title>
+			<div v-if="user">
+				<UploadPhotoModal v-if="profileUsername === user.username" :addNewPost="addNewPost" />
+				<div v-else>
+					<a-button v-if="!props.isFollowing" @click="followUser">Follow</a-button>
+					<a-button v-else @click="unfollowUser">Following</a-button>
+				</div>
+			</div>
 		</div>
 		<div class="bottom-content">
-			<a-typography-title :level="5">{{props.userInfo.posts}} posts</a-typography-title>
-			<a-typography-title :level="5">{{props.userInfo.followers}} followers</a-typography-title>
-			<a-typography-title :level="5">{{props.userInfo.following}} following</a-typography-title>
+			<a-typography-title :level="5">{{ props.userInfo.posts }} posts</a-typography-title>
+			<a-typography-title :level="5">{{ props.userInfo.followers }} followers</a-typography-title>
+			<a-typography-title :level="5">{{ props.userInfo.following }} following</a-typography-title>
 		</div>
 	</div>
 	<div v-else class="userbar-container">
